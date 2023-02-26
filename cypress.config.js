de5ffcb56862eed5f5e982c8406835216891c700
Jiří Galis <jiri@infinetcloud.com>
2023-02-27 10:20:01 +1300

    baseUrl update

--- cypress.config.js
+++ cypress.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://127.0.0.1:8080',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
 })
